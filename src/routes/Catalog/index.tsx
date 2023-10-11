02c53b031a16e241ed2cc20258f127546a5c82c3
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-11 18:40:03 -0300

    Search bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,22 @@
+import './styles.css';
 import Header from "../../components/Header";
+import SearchBar from '../../components/SearchBar';
 
 export default function Catalog(){
     return(
-        <Header />
+        <>
+            <Header />
+            <main className='ct-catalog-main'>
+                <section id="ct-catalog-section" className='ct-container'>
+                    <div className='ct-seach-bar-container'>
+                        <SearchBar />
+                    </div>
+                    <div className='ct-catalog-cards-container'>
+                        <div>card</div>
+                    </div>
+                </section>
+            </main>
+        </>
+        
     );
 }
